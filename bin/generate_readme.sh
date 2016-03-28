15d69e703977a75931189443877ace6624427be0
Javid Jamae <javidjamae@gmail.com>
2016-03-28 14:39:23 -0700

    added a comment to the generate_readme script to explain how the delimiter works

--- bin/generate_readme.sh
+++ bin/generate_readme.sh
@@ -5,4 +5,5 @@ VERSION=$(grep TOUT.EMBED_CODE_VERSION ${PROJECT_DIR}/src/index.js | cut -d"'" -
 # replacement in the subsequent sed command.
 MINIFIED_EMBED_CODE=$(cat ${PROJECT_DIR}/dist/tout-js-sdk-embed-code.min.js | sed 's/&/\\&/g')
 
+# The sed delimiters for the embed code are CTRL-A to avoid conflict with characters in the replacement code itself. You can use a CTRL-A in the code by typing CTRL + V + A
 sed -e"s##MINIFIED_JS_SDK_EMBED_CODE##$MINIFIED_EMBED_CODE" -e"s/##JS_SDK_EMBED_CODE_VERSION##/$VERSION/" <docs/README.md.template > README.md
